a3173eb59545c084f381fdc85b13c27a0a0296c7
busrasss <busrates.89@gmail.com>
2022-04-24 19:32:02 +0300

    initial step

---
+++ script.js
@@ -0,0 +1,54 @@
+const container= document.querySelector('.container');
+const count = document.getElementById('count');
+const amount = document.getElementById('amount');
+const select = document.getElementById('movie');
+const seats= document.querySelectorAll('.seat:not(.reserved)');
+
+container.addEventListener('click', function(e){
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
+        e.target.classList.toggle('selected');
+        calculateTotal()
+     
+
+
+
+    }
+});
+
+select.addEventListener('change', function(e){
+    calculateTotal();
+})
+
+function calculateTotal (){
+
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+    
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat){
+
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+
+    let selectedSeatIndex = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+
+
+    
+    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+    count.innerText = selectedSeatCount; 
+    amount.innerText= selectedSeatCount * select.value;
+
+    saveToLocalStorage(selectedSeatIndex);
+}
+
+function saveToLocalStorage(index){
+    localStorage.setItem('selectedSeats', JSON.stringify);
+}
